7850f84f78d41d4f157b7b6351e5b59787db5d5d
Carpenter James <jamesracer@optonline.net>
2026-01-20 10:45:10 -0500

    error2

--- Source/Carpenter415/Carpenter415Projectile.h
+++ Source/Carpenter415/Carpenter415Projectile.h
@@ -22,6 +22,12 @@ class ACarpenter415Projectile : public AActor
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	UProjectileMovementComponent* ProjectileMovement;
 
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ballMesh;
+
+	UPROPERTY(EditAnywhere)
+	UMaterial* baseMat;
+
 public:
 	ACarpenter415Projectile();
 
