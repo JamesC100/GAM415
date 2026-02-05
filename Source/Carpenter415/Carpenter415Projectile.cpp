294ff5daf96b628092fa3269f3c67456af4a1348
Carpenter James <jamesracer@optonline.net>
2026-02-05 09:21:10 -0500

    module2

--- Source/Carpenter415/Carpenter415Projectile.cpp
+++ Source/Carpenter415/Carpenter415Projectile.cpp
@@ -6,6 +6,8 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "Kismet/GameplayStatics.h"
 #include "Components/DecalComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
 
 ACarpenter415Projectile::ACarpenter415Projectile() 
 {
@@ -61,6 +63,14 @@ void ACarpenter415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherA
 
 	if (OtherActor !=nullptr)
 	{
+		if (colorP)
+		{
+			UNiagaraComponent* particleComp = UNiagaraFunctionLibrary::SpawnSystemAttached(colorP, HitComp, NAME_None, FVector(-20.f, 0.f, 0.f), FRotator(0.f), EAttachLocation::KeepRelativeOffset, true);
+			particleComp->SetNiagaraVariableLinearColor(FString("RandomColor"), randColor);
+			ballMesh->DestroyComponent();
+			CollisionComp->BodyInstance.SetCollisionProfileName("NoCollision");
+		}
+
 		float frameNum = UKismetMathLibrary::RandomFloatInRange(0.f, 3.f);
 
 		auto Decal = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), baseMat, FVector(UKismetMathLibrary::RandomFloatInRange(20.f, 40.f)), Hit.Location, Hit.Normal.Rotation(), 0.f);
